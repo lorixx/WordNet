8157af667711210bf1ac7ed9a8f21c7f08260bde
Zhisheng Huang <zhuang@affinnova.com>
2013-04-01 15:49:32 -0400

    Changed using BFS module for single source SAP

--- src/SAP.java
+++ src/SAP.java
@@ -24,8 +24,8 @@ public class SAP {
         minStepsFromV = new int[G.V()];
         minStepsFromW = new int[G.V()];
 
-        changedForV = new Queue<Integer>();
-        changedForW = new Queue<Integer>();
+        changedForV = new Queue<Integer>(); // might be used in DeluxeBFS
+        changedForW = new Queue<Integer>(); // might be used in DeluxeBFS
 
         shortestLengthSoFar = -1;
         ancestor = -1;
@@ -35,7 +35,6 @@ public class SAP {
     public int length(int v, int w) {
 
         this.calculateResults(v, w);
-
         return shortestLengthSoFar;
     }
 
@@ -43,139 +42,20 @@ public class SAP {
     public int ancestor(int v, int w) {
 
         this.calculateResults(v, w);
-
         return ancestor;
-
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
 
-        // saved all target vertices into stacks
-        Stack<Integer> stackForV = new Stack<Integer>();
-        Stack<Integer> stackForW = new Stack<Integer>();
-        int first;
-        int second;
-        int ancestorForV;
-        int ancestorForW;
-        int totalLengthForV = 0;
-        int totalLengthForW = 0;
-
-        for (int a : v) {
-            stackForV.push(a);
-        }
-        for (int b : w) {
-            stackForW.push(b);
-        }
-
-        if (stackForV.size() < 1 || stackForW.size() < 1)
-            throw new IndexOutOfBoundsException();
-
-        // 1. calculate ancestor for stackForV
-
-        while (stackForV.size()!= 1) {
-            first = stackForV.pop();
-            second = stackForV.pop();
-            calculateResults(first, second);
-            if (shortestLengthSoFar == -1)
-                return -1;
-            else {
-                stackForV.push(ancestor);
-                totalLengthForV += shortestLengthSoFar;
-            }
-        }
-
-        ancestorForV = stackForV.pop();
-
-        // 2. calculate ancestor for stackForW
-        while (stackForW.size()!= 1) {
-            first = stackForW.pop();
-            second = stackForW.pop();
-            calculateResults(first, second);
-            if (shortestLengthSoFar == -1)
-                return -1;
-            else {
-                stackForW.push(ancestor);
-                totalLengthForW += shortestLengthSoFar;
-            }
-        }
-
-        ancestorForW = stackForW.pop();
-
-        // 3. calculate the last two ancestors for two sub-graphs
-        calculateResults(ancestorForV, ancestorForW);
-
-        if (shortestLengthSoFar != -1) {
-
-            StdOut.println("The ancestor is " + ancestor);
-            return shortestLengthSoFar + totalLengthForV + totalLengthForW;
-        }
-
+        calculateResults(v, w);
         return shortestLengthSoFar;
     }
 
     // a common ancestor that participates in shortest ancestral path; -1 if no such path
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-        // saved all target vertices into stacks
-        Stack<Integer> stackForV = new Stack<Integer>();
-        Stack<Integer> stackForW = new Stack<Integer>();
-        int first;
-        int second;
-        int ancestorForV;
-        int ancestorForW;
-        int totalLengthForV = 0;
-        int totalLengthForW = 0;
 
-        for (int a : v) {
-            stackForV.push(a);
-        }
-        for (int b : w) {
-            stackForW.push(b);
-        }
-
-        if (stackForV.size() < 1 || stackForW.size() < 1)
-            throw new IndexOutOfBoundsException();
-
-        // 1. calculate ancestor for stackForV
-
-        while (stackForV.size()!= 1) {
-            first = stackForV.pop();
-            second = stackForV.pop();
-            calculateResults(first, second);
-            if (shortestLengthSoFar == -1)
-                return -1;
-            else {
-                stackForV.push(ancestor);
-                totalLengthForV += shortestLengthSoFar;
-            }
-        }
-
-        ancestorForV = stackForV.pop();
-
-        // 2. calculate ancestor for stackForW
-        while (stackForW.size()!= 1) {
-            first = stackForW.pop();
-            second = stackForW.pop();
-            calculateResults(first, second);
-            if (shortestLengthSoFar == -1)
-                return -1;
-            else {
-                stackForW.push(ancestor);
-                totalLengthForW += shortestLengthSoFar;
-            }
-        }
-
-        ancestorForW = stackForW.pop();
-
-        // 3. calculate the last two ancestors for two sub-graphs
-        calculateResults(ancestorForV, ancestorForW);
-
-        if (ancestor != -1) {
-
-            StdOut.println("The ancestor is " + ancestor);
-            return ancestor;
-        }
-
+        calculateResults(v, w);
         return ancestor;
     }
 
@@ -208,7 +88,6 @@ public class SAP {
         }
 
         this.cleanup();
-
         BreadthFirstDirectedPaths bfsForV = new BreadthFirstDirectedPaths(graph, v);
         BreadthFirstDirectedPaths bfsForW = new BreadthFirstDirectedPaths(graph, w);
 
@@ -216,7 +95,40 @@ public class SAP {
         for (int i = 0; i < graph.V(); i++) {
 
             if (bfsForV.hasPathTo(i) && bfsForW.hasPathTo(i)) {
-                StdOut.println(i);
+                //StdOut.println(i);
+                if (shortestLengthSoFar == -1) {
+                    shortestLengthSoFar = bfsForV.distTo(i) + bfsForW.distTo(i);
+                    ancestor = i;
+                } else if (bfsForV.distTo(i) + bfsForW.distTo(i) < shortestLengthSoFar) {
+                    shortestLengthSoFar = bfsForV.distTo(i) + bfsForW.distTo(i);
+                    ancestor = i;
+                } else
+                    continue;
+            }
+        }
+    }
+
+    private void calculateResults(Iterable<Integer> v, Iterable<Integer> w) {
+
+        if (v == null || w == null) throw new NullPointerException();
+
+        for (int a : v) {
+            if (a < 0 || a >= graph.V() )
+                throw new java.lang.IndexOutOfBoundsException();
+        }
+        for (int b : w) {
+            if (b < 0 || b >= graph.V() )
+                throw new java.lang.IndexOutOfBoundsException();
+        }
+
+        this.cleanup();
+        BreadthFirstDirectedPaths bfsForV = new BreadthFirstDirectedPaths(graph, v);
+        BreadthFirstDirectedPaths bfsForW = new BreadthFirstDirectedPaths(graph, w);
+
+        for (int i = 0; i < graph.V(); i++) {
+
+            if (bfsForV.hasPathTo(i) && bfsForW.hasPathTo(i)) {
+                //StdOut.println(i);
                 if (shortestLengthSoFar == -1) {
                     shortestLengthSoFar = bfsForV.distTo(i) + bfsForW.distTo(i);
                     ancestor = i;
@@ -229,85 +141,6 @@ public class SAP {
         }
 
 
-
-//
-//        this.cleanup();
-//        Queue<Integer> queueForV = new Queue<Integer>();
-//        queueForV.enqueue(v);
-//        markedByV[v] = true;
-//        minStepsFromV[v] = 0;
-//        changedForV.enqueue(v);
-//
-//        Queue<Integer> queueForW = new Queue<Integer>();
-//        queueForW.enqueue(w);
-//        markedByW[w] = true;
-//        minStepsFromW[w] = 0;
-//        changedForW.enqueue(w);
-//
-//        int currentStep = 0;
-//
-//        while (!queueForV.isEmpty() || !queueForW.isEmpty()) {
-//
-//            currentStep++;
-//            if (!queueForV.isEmpty()) {
-//                int currentV = queueForV.dequeue();
-//                for (int a : graph.adj(currentV)) {
-//
-//                    if (!markedByV[a]) {
-//                        //StdOut.println("a: " + a);
-//                        changedForV.enqueue(a);
-//                        markedByV[a] = true;
-//                        minStepsFromV[a] = currentStep; // update current step
-//                        queueForV.enqueue(a);
-//
-//                        //check whether it is marked by w
-//                        if (markedByW[a]) {
-//                            if (shortestLengthSoFar == -1) {
-//                                shortestLengthSoFar = minStepsFromV[a] + minStepsFromW[a];
-//                                ancestor = a;
-//                            } else if ( minStepsFromV[a] + minStepsFromW[a] < shortestLengthSoFar) {
-//                                shortestLengthSoFar = minStepsFromV[a] + minStepsFromW[a];
-//                                ancestor = a;
-//                            } else if (shortestLengthSoFar!= -1 && minStepsFromV[a] > shortestLengthSoFar) {
-//                                //stop searching
-//                                while(!queueForV.isEmpty()) queueForV.dequeue();
-//                                break;
-//                            }
-//                        }
-//                    }
-//                } // end of for
-//            }
-//
-//            if (!queueForW.isEmpty()) {
-//                int currentW = queueForW.dequeue();
-//                for (int b : graph.adj(currentW)) {
-//
-//                    if (!markedByW[b]) {
-//                        //StdOut.println("b: " + b);
-//                        changedForW.enqueue(b);
-//                        markedByW[b] = true;
-//                        minStepsFromW[b] = currentStep; // update current step
-//                        queueForW.enqueue(b);
-//
-//                        //check whether it is marked by w
-//                        if (markedByV[b]) {
-//
-//                            if (shortestLengthSoFar == -1) {
-//                                shortestLengthSoFar = minStepsFromV[b] + minStepsFromW[b];
-//                                ancestor = b;
-//                            } else if ( minStepsFromV[b] + minStepsFromW[b] < shortestLengthSoFar) {
-//                                shortestLengthSoFar = minStepsFromV[b] + minStepsFromW[b];
-//                                ancestor = b;
-//                            } else if (shortestLengthSoFar!= -1 && minStepsFromW[b] > shortestLengthSoFar) {
-//                                //stop searching
-//                                while(!queueForW.isEmpty()) queueForW.dequeue();
-//                                break;
-//                            }
-//                        }
-//                    }
-//                } // end of for
-//            }
-//        }
     }
 
 
@@ -319,7 +152,8 @@ public class SAP {
         listW.add(9); listW.add(11);
 
 
-        StdOut.println(this.length(listV, listW));
+        StdOut.println("length is " + this.length(listV, listW));
+        StdOut.println("ancestor is " + this.ancestor(listV, listW));
         assert this.length(listV, listW) == 8;
     }
 
@@ -331,7 +165,8 @@ public class SAP {
         listW.add(9); listW.add(5);
 
 
-        StdOut.println(this.length(listV, listW));
+        StdOut.println("length is " + this.length(listV, listW));
+        StdOut.println("ancestor is " + this.ancestor(listV, listW));
         assert this.length(listV, listW) == 4 : "This should be 4";
     }
 
@@ -343,7 +178,8 @@ public class SAP {
         listW.add(1); listW.add(7);
 
 
-        StdOut.println(this.length(listV, listW));
+        StdOut.println("length is " + this.length(listV, listW));
+        StdOut.println("ancestor is " + this.ancestor(listV, listW));
         assert this.length(listV, listW) == 2 : "This should be 2";
     }
 
