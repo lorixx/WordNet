937ebfdaa5b75632131bc0870d617fc7baad884e
Zhisheng Huang <zhuang@affinnova.com>
2013-04-01 17:49:07 -0400

    Filled up the implementation for WordNet

--- src/WordNet.java
+++ src/WordNet.java
@@ -1,12 +1,12 @@
 public class WordNet {
-    
+
     private ST<String, Integer> nounTable;  // keep track of noun, lookup from String
     private ST<Integer, String> synset;     // keep track of synset from id
 
     private Digraph graph;
     private SAP sap;
-    
-    
+
+
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
         this.nounTable = new ST<String, Integer>();
@@ -19,11 +19,11 @@ public class WordNet {
             size++;
             String currentLine = in.readLine();
 
-            String [] allArgs = currentLine.split(",");
+            String[] allArgs = currentLine.split(",");
             Integer id = Integer.parseInt(allArgs[0]);
 
             synset.put(id, allArgs[1]);
-            String [] nouns = allArgs[1].split(" ");
+            String[] nouns = allArgs[1].split(" ");
             for (String noun : nouns) {
                 nounTable.put(noun, id);
             }
@@ -35,17 +35,23 @@ public class WordNet {
 
         while (in.hasNextLine()) {
             String currentLine = in.readLine();
-            String [] numbers = currentLine.split(",");
+            String[] numbers = currentLine.split(",");
             Integer v = Integer.parseInt(numbers[0]);
             Integer w = Integer.parseInt(numbers[1]);
 
-            if (v < 0 || w < 0 || v >= size || w >= size)
+            if (v < 0 || w < 0 || v >= size || w >= size) {
                 throw new IndexOutOfBoundsException();
+            }
 
             this.graph.addEdge(v, w);
         }
 
         // check the graph is valid rooted DAG or not
+        if (!isRootedDAG(graph)) {
+            throw new IllegalArgumentException();
+        }
+
+        sap = new SAP(graph);
     }
 
     // returns all WordNet nouns
@@ -61,25 +67,63 @@ public class WordNet {
     // distance between nounA and nounB (defined below)
     public int distance(String nounA, String nounB) {
 
-        if (!this.isNoun(nounA) || !this.isNoun(nounB))
+        if (!this.isNoun(nounA) || !this.isNoun(nounB)) {
             throw new IllegalArgumentException();
+        }
 
-        return -1;
+        Integer idForA = nounTable.get(nounA);
+        Integer idForB = nounTable.get(nounB);
+        return sap.length(idForA, idForB);
     }
 
     // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
     // in a shortest ancestral path (defined below)
     public String sap(String nounA, String nounB) {
-        if (!this.isNoun(nounA) || !this.isNoun(nounB))
+        if (!this.isNoun(nounA) || !this.isNoun(nounB)) {
             throw new IllegalArgumentException();
+        }
 
-        return null;
+        Integer idForA = nounTable.get(nounA);
+        Integer idForB = nounTable.get(nounB);
+        int sapId = sap.length(idForA, idForB);
+        return synset.get(sapId);
     }
+
+    private boolean isRootedDAG(Digraph graph) {
+
+        DirectedCycle cycleFinder = new DirectedCycle(graph);
+        if (cycleFinder.hasCycle()) {
+            return false;
+        }
+
+        int numberOfZeroOut = 0;
+        for (int i = 0; i < graph.V(); i++) {
+            int count = 0;
+            for (int adjNode : graph.adj(i)) {
+                count++;
+            }
+
+            if (count == 0) {
+                numberOfZeroOut++;
+            }
+        }
+
+        if (numberOfZeroOut != 1) {
+            return false;
+        }
+
+        return true;
+    }
+
 
     // for unit testing of this class
     public static void main(String[] args) {
-        
+
+        WordNet wordNet = new WordNet(args[0], args[1]);
+
+        StdOut.println(wordNet.isNoun("a"));
+
     }
 
-    
+
 }
