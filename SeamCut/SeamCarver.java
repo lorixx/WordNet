e3417e5e6c204ee0fb0977ae07b63495da4b7cf2
Zhisheng Huang <lorixxhuang@gmail.com>
2013-04-08 00:32:16 -0400

    Create SeamCarver.java

---
+++ SeamCut/SeamCarver.java
@@ -0,0 +1,48 @@
+public class SeamCarver {
+
+    public SeamCarver(Picture picture) {
+
+    }
+    public Picture picture()                       // current picture
+    {
+        return null;
+    }
+    public     int width()                         // width of current picture
+    {
+        return 0;
+
+    }
+    public     int height()                        // height of current picture
+    {
+        return 0;
+
+    }
+    public  double energy(int x, int y)            // energy of pixel at column x and row y
+    {
+        return 0;
+
+    }
+    public   int[] findHorizontalSeam()            // sequence of indices for horizontal seam
+    {
+        return null;
+
+    }
+    public   int[] findVerticalSeam()              // sequence of indices for vertical seam
+    {
+        return null;
+
+    }
+    public    void removeHorizontalSeam(int[] a)   // remove horizontal seam from picture
+    {
+
+    }
+    public    void removeVerticalSeam(int[] a)     // remove vertical seam from picture
+    {
+
+    }
+    public static void main(String[] args) {
+        StdOut.println("haha");
+    }
+
+
+}
