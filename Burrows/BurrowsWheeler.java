89bcdfbe599d734be978c775225d469bc5db31d7
Zhisheng Huang <lorixxhuang@gmail.com>
2013-04-28 16:39:52 -0400

    encode function is implemented for BurrowsWheeler.java

--- Burrows/BurrowsWheeler.java
+++ Burrows/BurrowsWheeler.java
@@ -8,6 +8,31 @@
 public class BurrowsWheeler {
     // apply Burrows-Wheeler encoding, reading from standard input and writing to standard output
     public static void encode() {
+        String s = BinaryStdIn.readString();
+        CircularSuffixArray circularSuffixArray = new CircularSuffixArray(s);
+
+        int[] suffixArray = new int[circularSuffixArray.length()];
+        char[] charArray = new char[circularSuffixArray.length()];
+        int first = 0;
+
+        for (int i = 0; i < suffixArray.length; i++) {
+            if (circularSuffixArray.index(i) == 0)
+                first = i;
+
+            char lastChar;
+            if (circularSuffixArray.index(i) == 0) lastChar = s.charAt(s.length() - 1);
+            else lastChar = s.charAt(circularSuffixArray.index(i) - 1);
+
+            charArray[i] = lastChar;
+
+        }
+
+        BinaryStdOut.write(first);
+
+        for (char c : charArray)
+            BinaryStdOut.write(c);
+
+        BinaryStdOut.close();
 
     }
 
@@ -16,9 +41,22 @@ public class BurrowsWheeler {
 
     }
 
+    private char getChar( String originalString, int startSuffixForOrigin, int index) {
+
+        if ( index < originalString.length() - startSuffixForOrigin)
+            return originalString.charAt(startSuffixForOrigin + index);
+        else
+            return originalString.charAt(index - (originalString.length() - startSuffixForOrigin));
+
+    }
+
     // if args[0] is '-', apply Burrows-Wheeler encoding
     // if args[0] is '+', apply Burrows-Wheeler decoding
     public static void main(String[] args) {
-
+        if (args[0].equals("-")) {
+            BurrowsWheeler.encode();
+        } else if (args[0].equals("+")) {
+            BurrowsWheeler.decode();
+        }
     }
 }
