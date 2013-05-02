8090d0c7244fa8a7bfba094065c563ba3878b54e
Zhisheng Huang <zhuang@affinnova.com>
2013-05-02 16:26:12 -0400

    Start working on Palindrome patition problem, it seems to be a Dynamic Programming problem.

---
+++ src/leetcode/Palindrome.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhuang
+ * Date: 5/2/13
+ * Time: 3:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Palindrome {
+
+    public static int minCut(String s) {
+        return 0;
+    }
+}
