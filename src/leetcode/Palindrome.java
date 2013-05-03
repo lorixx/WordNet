e1d8b86943d9291297f858659a38bab9ef455fb8
Zhisheng Huang <lorixxhuang@gmail.com>
2013-05-02 23:04:46 -0400

    Implemented Dynamic Programming Palindrome

--- src/leetcode/Palindrome.java
+++ src/leetcode/Palindrome.java
@@ -9,7 +9,63 @@ package leetcode;
  */
 public class Palindrome {
 
+    public static boolean isPalindrome(String s) {
+        int start = 0, end = s.length() - 1;
+        while (start < end) {
+            if (s.charAt(start++) != s.charAt(end--)) return false;
+        }
+        return true;
+    }
+
     public static int minCut(String s) {
-        return 0;
+
+        if (s == null) return 0;
+        if (s.length() <= 1) return 0;
+
+        int length = s.length();
+        int[][] table = new int[length][length];
+        int min;
+
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                if (i == j)
+                    table[i][j] = 0;
+                else
+                    table[i][j] = -1;
+            }
+        }
+
+        for (int size = 2; size <= length; size++) {
+            for (int i = 0; i < length - size + 1; i++) {
+                int j = i + size - 1; // since we start from 0, need to minus 1 for array index
+                if (isPalindrome(s.substring(i, j + 1))) {
+                    table[i][j] = 0;
+                    continue;
+                }
+
+                min = 999999;
+                for (int k = i; k < j; k++) {
+                    if (min > table[i][k] + table[k + 1][j] + 1) {
+                        min = table[i][k] + table[k + 1][j] + 1;
+                    }
+                }
+                table[i][j] = min;
+            }
+        }
+
+//        for (int i = 0; i < length; i++) {
+//            for (int j = 0; j < length; j++) {
+//                System.out.printf( "%4d  ",  table[i][j] );
+//            }
+//            System.out.println();
+//        }
+
+
+        return table[0][length - 1];
+    }
+
+    public static void main(String[] args) {
+        //System.out.println(Palindrome.minCut("abcbammakakss"));
+        System.out.println(Palindrome.minCut(null));
     }
 }
