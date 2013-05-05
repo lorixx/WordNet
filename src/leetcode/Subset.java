2cc2d63d4819837870b77e18267e0a39f9a95125
Zhisheng Huang <lorixxhuang@gmail.com>
2013-05-05 17:43:20 -0400

    Subset problem is solved.

--- src/leetcode/Subset.java
+++ src/leetcode/Subset.java
@@ -1,6 +1,8 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Queue;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,9 +13,52 @@ import java.util.ArrayList;
  */
 public class Subset {
 
+    /**
+     * The idea behind this is that we need to understand what the real behavior among all these combinations
+     * keep a "visited" queue/arrayList to keep track what we have visited, then append the new character into the visited.
+     *
+     * The hint is from PIE: Recursion chapter.
+     *
+     * @param S
+     * @return
+     */
     public ArrayList<ArrayList<Integer>> subsets(int[] S) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        return null;
+
+        int[] array = Arrays.copyOf(S, S.length);
+
+        Arrays.sort(array);
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        ArrayList<ArrayList<Integer>> visited = new ArrayList<ArrayList<Integer>>();
+
+        for (int i = array.length - 1; i >= 0; i--) {
+
+            ArrayList<Integer> single = new ArrayList<Integer>();
+            single.add(array[i]);
+            result.add(single);
+
+            ArrayList<ArrayList<Integer>> recordForVisited = new ArrayList<ArrayList<Integer>>();
+            for (ArrayList<Integer> currentArray : visited) {
+                ArrayList<Integer> temp = new ArrayList<Integer>();
+                temp.add(array[i]); // append the current character
+                for (int j : currentArray) {
+                    temp.add(j);
+                }
+                result.add(temp); //add to result
+                recordForVisited.add(temp); // add to record For visit
+            }
+
+            visited.add(single);  // add the single to the visite
+            for (ArrayList<Integer> rest : recordForVisited) {
+                visited.add(rest);
+            }
+        }
+
+        ArrayList<Integer> empty = new ArrayList<Integer>();
+        result.add(empty);
+
+
+        return result;
     }
 }
