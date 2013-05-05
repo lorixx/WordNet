d8f3654d8edb65b2da390a83df163cf54591a4f2
Zhisheng Huang <lorixxhuang@gmail.com>
2013-05-04 20:51:53 -0400

    Passing Palindrome partitioning for the first time, meeting the time complexity! using 2 dynamic programming technique

--- src/leetcode/Palindrome.java
+++ src/leetcode/Palindrome.java
@@ -29,45 +29,99 @@ public class Palindrome {
      */
     public static int minCut(String s) {
 
-        if (s == null) return 0;
-        if (s.length() <= 1) return 0;
-
-        int length = s.length();
-        int[][] table = new int[length][length];
-        int min;
-
-//        for (int i = 0; i < length; i++) {
-//            for (int j = 0; j < length; j++) {
-//                if (i == j)
+//        if (s == null) return 0;
+//        if (s.length() <= 1) return 0;
+//
+//        int length = s.length();
+//        int[][] table = new int[length][length];
+//        int min;
+//
+//        for (int size = 1; size <= length; size++) {
+//            for (int i = 0; i < length - size + 1; i++) {
+//                int j = i + size - 1; // since we start from 0, need to minus 1 for array index
+//                if (isPalindrome(s.substring(i, j + 1))) {
 //                    table[i][j] = 0;
-//                else
-//                    table[i][j] = -1;
+//                    continue;
+//                }
+//
+//                min = 999999;
+//                for (int k = i; k < j; k++) {
+//                    if (min > table[i][k] + table[k + 1][j] + 1) {
+//                        min = table[i][k] + table[k + 1][j] + 1;
+//                    }
+//                }
+//                table[i][j] = min;
 //            }
 //        }
+//
+//        return table[0][length - 1];
 
-        for (int size = 1; size <= length; size++) {
-            for (int i = 0; i < length - size + 1; i++) {
-                int j = i + size - 1; // since we start from 0, need to minus 1 for array index
-                if (isPalindrome(s.substring(i, j + 1))) {
-                    table[i][j] = 0;
-                    continue;
-                }
+        int length = s.length();
+        int[] minCut = new int[length];
+        boolean[][] isPalindrome = new boolean[length][length];
 
-                min = 999999;
-                for (int k = i; k < j; k++) {
-                    if (min > table[i][k] + table[k + 1][j] + 1) {
-                        min = table[i][k] + table[k + 1][j] + 1;
-                    }
-                }
-                table[i][j] = min;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                if (i == j) isPalindrome[i][j] = true;
             }
         }
 
-        return table[0][length - 1];
+        // minCut is how many cut from 0 to i, [0, i] is the area that we use
+        for (int i = 0; i < length; i++) {
+            minCut[i] = i;
+        }
+        minCut[0] = 0;
+
+        for (int size = 1; size <= length; size++) {
+
+            if (size > 1 && s.charAt(0) == s.charAt(size - 1) && (isPalindrome[1][size - 2] || size < 3)) {
+                minCut[size - 1] = 0;
+                isPalindrome[0][size - 1] = true;
+                continue;
+            }
+
+            for (int i = 0; i < size - 1; i++) {  //size minus 1 due to the edge
+
+                int leftCut = minCut[i];
+                int rightCut, total;
+
+
+
+                if (size - 1 - i == 1) {
+                    rightCut = 0;   // if it is one character, then it is palindrome
+                    total = leftCut + rightCut + 1;
+                    if (minCut[size - 1] > total) {
+                        minCut[size - 1] = total;
+                    }
+                } else if (size - 1 - i == 2 && s.charAt(i + 1) == s.charAt(size - 1))  {
+                    rightCut = 0;
+                    isPalindrome[i + 1][size - 1] = true;
+                    total = leftCut + rightCut + 1;
+                    if (minCut[size - 1] > total) {
+                        minCut[size - 1] = total;
+                    }
+                }
+                else if (s.charAt(i + 1) == s.charAt(size - 1) && isPalindrome[i + 2][size - 2]) {
+                    rightCut = 0;
+                    isPalindrome[i + 1][size - 1] = true;
+                    total = leftCut + rightCut + 1;
+                    if (minCut[size - 1] > total) {
+                        minCut[size - 1] = total;
+                    }
+                }
+
+
+            }
+        }
+
+        return minCut[length - 1];
+
     }
 
     public static void main(String[] args) {
-        //System.out.println(Palindrome.minCut("abcbammakakss"));
+        System.out.println(Palindrome.minCut("abcbammakakss"));
+        System.out.println(Palindrome.minCut("aaa"));
+
         long start = System.nanoTime();
         System.out.println(Palindrome.minCut("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
         long duration = System.nanoTime() - start;
