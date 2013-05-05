8a3a8b9263d5c401c6deb6d6c3738b144983547c
Zhisheng Huang <lorixxhuang@gmail.com>
2013-05-05 15:00:23 -0400

    Tried work on Subset, study PIE for maintaining tail pointer for delete and insertAfter in a linked list, implemented both recursive and iterative version for Binary Search.

---
+++ src/leetcode/Subset.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Zhisheng
+ * Date: 5/5/13
+ * Time: 2:29 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Subset {
+
+    public ArrayList<ArrayList<Integer>> subsets(int[] S) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        return null;
+    }
+}
