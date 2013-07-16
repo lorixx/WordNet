dde563ec1220f95d0cfef5b310fee8049788d46e
Zhisheng Huang <lorixxhuang@gmail.com>
2013-07-15 22:49:57 -0400

    Added interview question for finding nth node from the end, use windows sliding technique

---
+++ src/interview/FindNthNodeFromEnd.java
@@ -0,0 +1,67 @@
+package interview;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Zhisheng
+ * Date: 7/15/13
+ * Time: 9:04 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FindNthNodeFromEnd {
+
+    public class ListNode {
+        int item;
+        ListNode next;
+
+        public ListNode(int v, ListNode next) {
+            this.next = next;
+            item =v;
+        }
+    }
+
+    public ListNode findNthFromTheEnd(ListNode head, int k) {
+
+        if (head == null) return null;
+
+        if (k < 1) return null;
+
+        ListNode currentNode = head;
+
+        for (int i = 0; i < k; i++) {
+            if (currentNode.next == null) return null;
+            currentNode = currentNode.next;
+        }
+
+        ListNode start = head;
+        ListNode end = currentNode;
+
+        while (end != null) {
+
+            if (end.next == null) { // if it is the end
+                return start;
+            }
+            end = end.next;
+            start = start.next;
+        }
+
+        return null;
+
+    }
+
+    public void test() {
+        ListNode a = new ListNode(5, null);
+        ListNode b = new ListNode(4, a);
+        ListNode c = new ListNode(3, b);
+        ListNode d = new ListNode(2, c);
+        ListNode e = new ListNode(1, d);
+        ListNode f = new ListNode(0, e);
+
+        System.out.println(this.findNthFromTheEnd(f, 6).item);
+    }
+
+    public static void main(String[] args) {
+        FindNthNodeFromEnd findNthNodeFromEnd = new FindNthNodeFromEnd();
+        findNthNodeFromEnd.test();
+    }
+
+}
