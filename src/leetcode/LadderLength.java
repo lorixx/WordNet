ecd45c3eebc21c40dbf171a00162e243381c89bc
Zhisheng Huang <lorixxhuang@gmail.com>
2013-07-18 02:34:24 -0400

    working on ladder length, still need to handle visited nodes.

--- src/leetcode/LadderLength.java
+++ src/leetcode/LadderLength.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 
 /**
@@ -12,7 +14,55 @@ import java.util.HashSet;
 public class LadderLength {
 
     public int ladderLength(String start, String end, HashSet<String> dict) {
-         return 0;
+
+
+        if (start.equals(end)) return 0;
+
+        if (dict.size() < 1) return 0;
+
+        ArrayList<String> queue =  new ArrayList<String>();  // Use BFS
+        HashMap<String, Integer> map = new HashMap<String, Integer>(); // use to track the level
+
+        queue.add(start);
+        map.put(start, 0);
+
+        while (!queue.isEmpty()) {
+            String currentString = queue.remove(0);
+
+            for (int i = 0; i < currentString.length(); i++) {
+
+                for (int j = 0; j < 26; j++) {
+                    StringBuilder sb = new StringBuilder(currentString);
+                    sb.setCharAt(i, (char)('a' + j));
+                    String newString = sb.toString();
+
+                    if (newString.equals(end))
+                        return map.get(currentString) + 1; // found the result
+
+                    if (dict.contains(newString) && !newString.equals(currentString)) {
+                        queue.add(newString);
+                        map.put(newString, map.get(currentString) + 1);
+                    }
+                }
+            }
+        }
+
+        return 0;
+
+    }
+
+    public  static void main(String[] args) {
+
+        HashSet<String> set = new HashSet<String>();
+        set.add("hot");
+        set.add("dtg");
+        set.add("hog");
+
+
+        LadderLength ladderLength = new LadderLength();
+
+        System.out.println(ladderLength.ladderLength("hot", "atg", set));
+
     }
 
 }
